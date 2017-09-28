5103f112c1ac79b8b702c94189f24bf43e1424f6
SunnyGoswami <sunnydgoswami@gmail.com>
2017-09-28 15:17:19 +0000

    added create exercise model, controller, and partial views for _form and _exercise. Updated workout/show.html.haml to include exercise partials.

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -1,4 +1,5 @@
 class Workout < ActiveRecord::Base
+    has_many :exercises
     # attr_accessor :date, :workout, :mood, :length
     # validates :workout, :presence => { :message => "enter something" }
 end
