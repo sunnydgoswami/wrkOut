7c01991ad45506a03e56f0b3441c20133ce1cedb
SunnyGoswami <sunnydgoswami@gmail.com>
2017-09-30 00:33:48 +0000

    css is working better for exercise show view

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -5,4 +5,12 @@ class ExercisesController < ApplicationController
         
         redirect_to workout_path(@workout)
     end
+    
+    # def destroy
+    #     @workout = Workout.find(params[:workout_id])
+    #     @exercises.destroy
+    #     flash[:success] = "Exercise Deleted!"
+    #     redirect_to workout_path(@workout)
+    # end    
+    
 end
